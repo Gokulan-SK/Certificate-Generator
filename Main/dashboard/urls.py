d188a1f2b086e902d4026f09b39c1f414a7f33b9
skgokulan2003@gmail.com <skgokulan2003@gmail.com>
2024-02-20 16:33:48 +0530

    modified project structure and added dashboard

--- Main/dashboard/urls.py
+++ Main/dashboard/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views  # Import Django's authentication views
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('about/', views.about_page, name='about'),
-    path('products/', views.products_page, name='products'),
-    path('services/', views.services_page, name='services'),
+    path('dashboard/',views.dashboard,name='dashboard'),
 ]
